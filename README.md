ec95f8a27d2f88781fcac4cec929acc9b3f0c3b8
Said Tahsin Dane <tasomaniac@gmail.com>
2015-10-14 22:49:44 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,50 @@
+IntegrationPreference
+=====================
+
+<!-- [![Android Arsenal](https://img.shields.io/badge/Android%20Arsenal-ContentLoadingProgressDialog-brightgreen.svg?style=flat)](http://android-arsenal.com/details/1/2117) -->
+[![Build Status](https://travis-ci.org/tasomaniac/IntegrationPreference.png?branch=master)](https://travis-ci.org/tasomaniac/IntegrationPreference)
+[![License](http://img.shields.io/:license-apache-blue.svg)](http://www.apache.org/licenses/LICENSE-2.0.html)
+
+This library is especially for extension apps developers. Some applications like (Muzei, Series Guide)
+has extension capabilities. You can use their SDK to integrate your app into theirs. Or your app may
+ need another app to be installed on the device.
+
+You put IntegrationPreference into your preference XML file and it checks the given intent to it.
+If it cannot find the application to handle that Intent, it opens Play Store on click to install the
+required application.
+
+Usage
+-----
+
+TODO
+
+Download
+--------
+
+```groovy
+compile 'com.tasomaniac:integrationpreference:0.1'
+```
+
+Snapshots of the development version are available in [Sonatype's `snapshots` repository][snap].
+
+License
+-------
+
+    Copyright (C) 2015 Said Tahsin Dane
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+
+
+
+ [snap]: https://oss.sonatype.org/content/repositories/snapshots/
---
+++ library/src/main/java/com/tasomaniac/android/widget/IntegrationPreference.java
@@ -0,0 +1,194 @@
+/*
+ * Copyright (c) 2015 Said Tahsin Dane
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tasomaniac.android.widget;
+
+import android.annotation.TargetApi;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.TypedArray;
+import android.net.Uri;
+import android.os.Build;
+import android.preference.CheckBoxPreference;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.util.AttributeSet;
+
+public class IntegrationPreference extends CheckBoxPreference {
+
+    PackageManager packageManager;
+
+    AppInstallEnabler appInstallEnabler;
+
+    Intent originalIntent;
+    Intent integrationIntent;
+    private int errorTextColor;
+
+    public IntegrationPreference(Context context) {
+        super(context);
+        initialize(context, null);
+    }
+
+    public IntegrationPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize(context, attrs);
+    }
+
+    public IntegrationPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize(context, attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public IntegrationPreference(
+            Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initialize(context, attrs);
+    }
+
+    private void initialize(Context context, AttributeSet attrs) {
+        packageManager = context.getPackageManager();
+        setDefaultValue(false);
+
+        final TypedArray sa = context.obtainStyledAttributes(attrs,
+                R.styleable.IntegrationPreference);
+
+        extractIntegrationIntent(sa);
+        errorTextColor = sa.getColor(R.styleable.IntegrationPreference_errorTextColor,
+                ContextCompat.getColor(context, R.color.error_color));
+
+        sa.recycle();
+
+        setDisableDependentsState(true);
+
+        appInstallEnabler = new AppInstallEnabler(context, this);
+    }
+
+    @Override
+    protected void onAttachedToActivity() {
+        super.onAttachedToActivity();
+        originalIntent = getIntent();
+        if (originalIntent != null && !hasIntent(integrationIntent)) {
+            integrationIntent = marketIntent(originalIntent);
+        }
+        checkState();
+    }
+
+    public void resume() {
+        appInstallEnabler.resume();
+    }
+
+    public void pause() {
+        appInstallEnabler.pause();
+    }
+
+    void checkState() {
+        if (hasIntent(originalIntent)) {
+            setChecked(false);
+        } else {
+            setChecked(true);
+            if (hasIntent(integrationIntent)) {
+                setIntent(integrationIntent);
+            }
+        }
+    }
+
+    private void extractIntegrationIntent(TypedArray sa) {
+        integrationIntent = new Intent();
+
+        integrationIntent.setAction(
+                sa.getString(R.styleable.IntegrationPreference_integrationIntentAction));
+
+        String data = sa.getString(R.styleable.IntegrationPreference_integrationIntentData);
+        String mimeType = sa.getString(R.styleable.IntegrationPreference_integrationIntentMimeType);
+        if (data != null) {
+            integrationIntent.setData(Uri.parse(data));
+        }
+        if (mimeType != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                integrationIntent.setTypeAndNormalize(mimeType);
+            } else {
+                integrationIntent.setType(mimeType);
+            }
+        }
+
+        String packageName =
+                sa.getString(R.styleable.IntegrationPreference_integrationIntentTargetPackage);
+        String className =
+                sa.getString(R.styleable.IntegrationPreference_integrationIntentTargetClass);
+        if (packageName != null && className != null) {
+            integrationIntent.setComponent(new ComponentName(packageName, className));
+        }
+    }
+
+    private Intent marketIntent(Intent originalIntent) {
+        ComponentName component = originalIntent.getComponent();
+        if (component != null) {
+            final String packageName = component.getPackageName();
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=" + packageName));
+            if (!hasIntent(marketIntent)) {
+                marketIntent.setData(Uri.parse(
+                        "https://play.google.com/store/apps/details?id=" + packageName));
+            }
+            if (hasIntent(marketIntent)) {
+                return marketIntent;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void setSummaryOn(CharSequence summary) {
+        if (summary != null) {
+            SpannableString summarySpan = getErrorString(summary);
+            super.setSummaryOn(summarySpan);
+        } else {
+            super.setSummaryOn(null);
+        }
+    }
+
+    private boolean hasIntent(@Nullable Intent intent) {
+        return intent != null
+                && packageManager.resolveActivity(intent, 0) != null;
+    }
+
+    @Override
+    protected void onClick() {
+    }
+
+    @Override
+    public void setChecked(boolean checked) {
+        super.setChecked(checked);
+
+        if (!checked) {
+            setTitle(getTitle().toString());
+        } else {
+            SpannableString titleSpan = getErrorString(getTitle());
+            setTitle(titleSpan);
+        }
+    }
+
+    private SpannableString getErrorString(CharSequence originalString) {
+        SpannableString errorSpan = new SpannableString(originalString);
+        errorSpan.setSpan(new ForegroundColorSpan(errorTextColor), 0, errorSpan.length(), 0);
+        return errorSpan;
+    }
+}
